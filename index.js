e5dde4047f22992a2cec030d6b6b82bb4969a63d
Dawid Płocki <dplocki@future-processing.com>
2019-09-05 14:27:00 +0200

    Removing the express-session package

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(session({
-    secret: process.env.SECRET || 'donttellanyone'
-}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/json'}));
 
-app.get("/", (req, res) => {
-    res.status(200).send("yeap, it is working");
+
+app.post("/", (req, res) => {
+    const body = req.body;
+    const droidName = body.droidId;
+    const message = body.message;
+
+    res.status(200).send([
+        droidName,
+        message
+    ]);
 });
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
