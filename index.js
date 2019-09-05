fe34f7742cddc43b7e4c7875d5a2cb46c9fc1f48
Dawid Płocki <dplocki@future-processing.com>
2019-09-05 14:03:59 +0200

    Adding: express-sessiona and body-parser module

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require("express");
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(session({
+    secret: process.env.SECRET || 'donttellanyone'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.status(200).send("yeap, it is working");
